5c892568907d9a03104982efbc1c666b005a60ed
Michael Schramm <michael@tucan.ai>
2026-05-07 10:51:15 +0200

    update format

--- src/types.ts
+++ src/types.ts
@@ -18,8 +18,7 @@ type RequireAtLeastOne<T, Keys extends keyof T = keyof T> = Pick<
   Exclude<keyof T, Keys>
 > &
   {
-    [K in Keys]-?: Required<Pick<T, K>> &
-      Partial<Pick<T, Exclude<Keys, K>>>;
+    [K in Keys]-?: Required<Pick<T, K>> & Partial<Pick<T, Exclude<Keys, K>>>;
   }[Keys];
 
 interface EmailData {
